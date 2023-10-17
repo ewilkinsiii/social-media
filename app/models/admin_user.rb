a92672753997073ab94cb8c7acda1c5a9060ecae
ewilkinsiii <ewilkinsiii@gmail.com>
2023-10-17 14:28:00 -0500

    added ransackable_attributes to admin_user and user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,13 +4,13 @@
 #
 # Table name: admin_users
 #
-#  id                     :bigint          not null, primary key
+#  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
-#  sign_in_count          :bigint          default(0), not null
+#  sign_in_count          :bigint           default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :inet
@@ -29,4 +29,9 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at current_sign_in_at current_sign_in_ip email id last_sign_in_at last_sign_in_ip
+       remember_created_at reset_password_sent_at sign_in_count updated_at]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@
 #
 # Table name: users
 #
-#  id                     :bigint          not null, primary key
+#  id                     :bigint           not null, primary key
 #  email                  :string
 #  encrypted_password     :string           default(""), not null
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  allow_password_change  :boolean          default(FALSE), not null
-#  sign_in_count          :bigint          default(0), not null
+#  sign_in_count          :bigint           default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :inet
@@ -42,11 +42,18 @@ class User < ApplicationRecord
 
   before_validation :init_uid
 
+  has_many :posts, dependent: :destroy
+
   def self.from_social_provider(provider, user_params)
     where(provider:, uid: user_params['id']).first_or_create! do |user|
       user.password = Devise.friendly_token[0, 20]
       user.assign_attributes user_params.except('id')
     end
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[first_name last_name username email created_at current_sign_in_at current_sign_in_ip email id last_sign_in_at last_sign_in_ip
+       remember_created_at reset_password_sent_at sign_in_count updated_at]
   end
 
   def full_name
